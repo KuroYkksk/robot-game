b9965f8c0a217f0bcdc229daa4c948ff9bae1bef
KuroYkksk <50042413+KuroYkksk@users.noreply.github.com>
2019-04-27 15:58:01 +0800

    Add files via upload

---
+++ 俄罗斯轮盘(可装填)/CQPdemo/src/MsgSub.cpp
@@ -0,0 +1,280 @@
+#include "MsgSub.h"
+#include "cqp.h"
+#include <time.h>
+#include <string>
+using namespace std;
+
+//导入全局变量 ac   在appmain.h 中定义
+extern int ac;
+
+
+int players = 0;			//玩家数
+bool ruroGame = false;		 //判断游戏是否开始的标志
+int64_t ruroPlayer[6] = { 0 };//参加轮盘的玩家
+bool bullet[6] = { 0 };			//子弹的位置，初始化为-1
+int n = -1;							//子弹数量
+int shootTurn = -1;			//开枪回合
+
+//游戏开始
+void ruroStart(int64_t group, string signal)
+{
+	ruroGame = true;
+
+	int load;		//装填位置
+
+	srand((unsigned)time(NULL));
+	if (signal == "*ruro start")
+	{
+		signal += "1";
+	}
+		sscanf(signal.c_str(), "*ruro start%d", &n);
+		//装填n发子弹
+		for (int i = 0; i < n; i++)
+		{
+			load = rand() % 6;
+			if (bullet[load] == false) {
+				bullet[load] = true;
+			}
+			else {
+				i--;
+			}
+		}
+
+	shootTurn = 0;
+	string ruroHint = "【心跳~禁言俄罗斯轮盘】游戏开始\n" + to_string(n) + "发子弹已装填~\n";
+	for (int i = 0; i < players; i++)
+	{
+		ruroHint += "[CQ:at,qq=" + to_string(ruroPlayer[i]) + "]";
+	}
+	CQ_sendGroupMsg(ac, group, ruroHint.c_str());
+}
+//游戏重置
+void ruroReset()
+{
+	ruroGame = false;
+	for (int i = 0; i < 6; i++)
+	{
+		ruroPlayer[i] = 0;
+	}
+	bullet[6] = { 0 };
+	players = 0;
+	shootTurn = -1;
+	n = -1;
+}
+
+//玩家乱序算法
+void Disorder(int64_t Player[], int player)
+{
+	int index, i;
+	int64_t tmp;
+	srand((unsigned)time(NULL));
+	for (i = 0; i < player; i++)
+	{
+		index = rand() % (player - i) + i;
+		tmp = Player[i];
+		Player[i] = Player[index];
+		Player[index] = tmp;
+	}
+}
+
+void GroupMsgSub::threadMain()
+{
+	while (!m_quit)
+	{
+		if (m_groupMsgBuffer.size() > 0)
+		{
+			m_mutex.lock();
+			GroupMsg msg = m_groupMsgBuffer.front();
+			m_groupMsgBuffer.pop();
+			m_mutex.unlock();
+
+			string ruroHint;			//发送游戏提示
+			bool ruroInclude = false;	//检查玩家是否已报名
+			//当第一个字符是*时读取指令
+			if (msg.msg.substr(0, 1) == "*")	
+			{
+				if (msg.msg == "*ruro join")
+				{
+					//当游戏未开始时
+					if (ruroGame == false) {
+
+						//检查玩家是否已报名
+						for (int i = 0; i < 6; i++)
+						{
+							if (ruroPlayer[i] == msg.fromQQ)
+							{
+								ruroInclude = true;
+								break;
+							}
+							ruroInclude = false;
+						}
+
+						if (ruroInclude)
+						{
+							CQ_sendGroupMsg(ac, msg.fromGroup, "【报名失败，你已报名】");
+						}
+						else if (players >= 6)
+						{
+							CQ_sendGroupMsg(ac, msg.fromGroup, "【报名失败，玩家数已满】");
+						}
+						else
+						{
+							players++;
+							ruroHint = "【报名成功，当前有" + to_string(players) + "位玩家】";
+							CQ_sendGroupMsg(ac, msg.fromGroup, ruroHint.c_str());
+							ruroPlayer[players - 1] = msg.fromQQ;	//将发送信息的QQ存入数组
+						}
+					}
+					else {
+						CQ_sendGroupMsg(ac, msg.fromGroup, "【报名失败，游戏已经开始】");
+					}
+				}
+				else if (msg.msg == "*ruro quit")
+				{
+					int ruroIndex = -1;
+					//检查玩家是否已报名
+					for (int i = 0; i < 6; i++)
+					{
+						if (ruroPlayer[i] == msg.fromQQ)
+						{
+							ruroInclude = true;
+							int ruroIndex = -1;
+							break;
+						}
+						ruroInclude = false;
+					}
+
+					if (ruroGame == true)
+					{
+						CQ_sendGroupMsg(ac, msg.fromGroup, "【取消报名失败，游戏已经开始】");
+					}
+					else if (ruroInclude)
+					{
+						//将被取消报名的玩家后面的每个玩家向前移动一位
+						for (int i = ruroIndex; i < 6; i++)
+						{
+							ruroPlayer[i] = i == 5 ? 0 : ruroPlayer[i + 1];
+						}
+						players--;
+						ruroHint = "【取消报名成功，当前有" + to_string(players) + "名玩家】";
+						CQ_sendGroupMsg(ac, msg.fromGroup, ruroHint.c_str());
+					}
+					else {
+						CQ_sendGroupMsg(ac, msg.fromGroup, "【取消报名失败，你没有报名】");
+					}
+				}
+				else if (msg.msg == "*ruro start6")
+				{
+					CQ_sendGroupMsg(ac, msg.fromGroup, "【子弹已满，请重新装填】");
+				}
+				else if (msg.msg == "*ruro start" || msg.msg == "*ruro start1" || msg.msg == "*ruro start2" || msg.msg == "*ruro start3" || msg.msg == "*ruro start4"|| msg.msg == "*ruro start5")
+				{
+					if (ruroGame == false) {
+						if (players < 1)
+						{
+							CQ_sendGroupMsg(ac, msg.fromGroup, "【开始失败，玩家不足】");
+						}
+						else {
+							Disorder(ruroPlayer, players);
+							ruroStart(msg.fromGroup, msg.msg);
+							ruroHint = "【首先由[CQ:at,qq=" + to_string(ruroPlayer[shootTurn]) + "]开枪】";
+							CQ_sendGroupMsg(ac, msg.fromGroup, ruroHint.c_str());
+						}
+					}
+					else {
+						CQ_sendGroupMsg(ac, msg.fromGroup, "【开始失败，游戏已经开始】");
+					}
+				}
+				else if (msg.msg == "*ruro reset")
+				{
+					ruroReset();
+					CQ_sendGroupMsg(ac, msg.fromGroup, "【俄罗斯轮盘重置完成】");
+				}
+				else if (msg.msg.substr(0, 2) == "**")
+				{
+					if (ruroGame == true) {
+						if (ruroPlayer[shootTurn] == msg.fromQQ)
+						{
+							if (msg.msg == "**shoot")
+							{
+								shootTurn++;
+								if (bullet[shootTurn - 1] == true) {
+									ruroHint = "【砰————————】\n[CQ:at,qq=" + to_string(ruroPlayer[shootTurn-1]) + "]\n被禁言1分钟";
+									CQ_setGroupBan(ac, msg.fromGroup, msg.fromQQ, 60);//禁言60s
+									CQ_sendGroupMsg(ac, msg.fromGroup, ruroHint.c_str());
+									n--;
+								}
+								else if (bullet[shootTurn - 1] == false) {
+									CQ_sendGroupMsg(ac, msg.fromGroup, "【什么都没发生~】");
+								}
+
+								if (shootTurn == players) {
+									CQ_sendGroupMsg(ac, msg.fromGroup, "【恭喜幸存者~~游戏结束】");
+									ruroReset();
+								}
+								else if (n == 0) {
+									CQ_sendGroupMsg(ac, msg.fromGroup, "【子弹耗尽~~游戏结束】");
+									ruroReset();
+								}
+								else {
+									ruroHint = "【轮到[CQ:at,qq=" + to_string(ruroPlayer[shootTurn]) + "]开枪】";
+									CQ_sendGroupMsg(ac, msg.fromGroup, ruroHint.c_str());
+								}
+							}
+						}
+						else {
+							CQ_sendGroupMsg(ac, msg.fromGroup, "【你未参加游戏，或尚未轮到你的回合】");
+						}
+					}
+					else {
+						CQ_sendGroupMsg(ac, msg.fromGroup, "【游戏尚未开始】");
+					}
+				}
+
+			}
+
+
+
+		}
+		else
+		{
+			ThreadBase::mSleep(10);
+		}
+	}
+	return;
+}
+
+GroupMsgSub::GroupMsgSub()
+{
+	m_quit = false;
+}
+
+GroupMsgSub::~GroupMsgSub()
+{
+}
+
+void GroupMsgSub::start()
+{
+	m_quit = false;
+	ThreadBase::start();
+}
+
+void GroupMsgSub::quite()
+{
+	ThreadBase::quit();
+	m_quit = true;
+}
+
+
+void GroupMsgSub::pushMsg(int32_t msgId, int64_t fromGroup, int64_t fromQQ, std::string msg)
+{
+	//如果缓存的消息大于100跳  则直接抛弃新到的消息
+	if (m_groupMsgBuffer.size() < 100)
+	{
+		m_mutex.lock();
+		m_groupMsgBuffer.push(GroupMsg(msgId, fromGroup, fromQQ, msg));
+		m_mutex.unlock();
+	}
+}
+
+
